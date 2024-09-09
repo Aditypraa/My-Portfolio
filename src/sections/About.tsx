3f7e2a93c5a95303771f9c1426c9cd5e9567694b
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-09 08:28:38 +0700

    refactor data hobbies and toolbox

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -3,117 +3,15 @@ import Card from "@/components/Card";
 import SectionHeader from "@/components/SectionHeader";
 import bookImage from "@/assets/images/book-cover.png";
 import Image from "next/image";
-import HtmlIcon from "@/assets/icons/html5.svg";
-import CssIcon from "@/assets/icons/css3.svg";
-import JavascriptIcon from "@/assets/icons/javascript.svg";
-import BoostrapIcon from "@/assets/icons/bootstrap.svg";
-import TailwindCssIcon from "@/assets/icons/tailwindcss.svg";
-import MysqlIcon from "@/assets/icons/mysql.svg";
-import MongodbIcon from "@/assets/icons/mongodb.svg";
-import GitIcons from "@/assets/icons/git.svg";
-import GithubIcon from "@/assets/icons/github.svg";
-import ExpressIcon from "@/assets/icons/express.svg";
-import NodeIcon from "@/assets/icons/nodejs.svg";
-import ReactIcon from "@/assets/icons/react.svg";
-import NextjsIcon from "@/assets/icons/nextjs.svg";
-import DockerIcon from "@/assets/icons/docker.svg";
-import GcpIcon from "@/assets/icons/googlecloud.svg";
-import LinuxIcon from "@/assets/icons/linux.svg";
-import UbuntuIcon from "@/assets/icons/ubuntu.svg";
-import WindowsIcon from "@/assets/icons/windows.svg";
 import mapImage from "@/assets/images/map.png";
 import smileMemoji from "@/assets/images/memoji-smile.png";
 import CardHeader from "@/components/CardHeader";
 import ToolboxItems from "@/components/ToolboxItems";
 import { motion } from "framer-motion";
 import { useRef } from "react";
+import { hobbies } from "@/data/hobbiesData";
+import { toolboxItems } from "@/data/toolboxData";
 
-const toolboxItems = [
-  {
-    title: "HTML",
-    iconType: HtmlIcon,
-  },
-  {
-    title: "CSS",
-    iconType: CssIcon,
-  },
-  {
-    title: "JavaScript",
-    iconType: JavascriptIcon,
-  },
-  {
-    title: "Bootstrap",
-    iconType: BoostrapIcon
-  },
-  {
-    title: 'TailwindCss',
-    iconType: TailwindCssIcon
-  },
-  {
-    title: 'MySQL',
-    iconType: MysqlIcon
-  },
-  {
-    title: 'MongoDB',
-    iconType: MongodbIcon
-  },
-  {
-    title: 'Git',
-    iconType: GitIcons
-  },
-  {
-    title: 'Github',
-    iconType: GithubIcon
-  },
-  {
-    title: 'ExpressJs',
-    iconType: ExpressIcon
-  },
-  {
-    title: 'NodeJs',
-    iconType: NodeIcon
-  },
-  {
-    title: 'ReactJs',
-    iconType: ReactIcon
-  },
-  {
-    title: 'NextJs',
-    iconType: NextjsIcon
-  },
-  {
-    title: 'Docker',
-    iconType: DockerIcon
-  },
-  {
-    title: 'Google Cloud Platform',
-    iconType: GcpIcon
-  },
-  {
-    title: 'Linux',
-    iconType: LinuxIcon
-  },
-  {
-    title: 'Ubuntu',
-    iconType: UbuntuIcon
-  },
-  {
-    title: 'Windows',
-    iconType: WindowsIcon
-  }
-];
-
-const hobbies = [
-  { title: "Football", emoji: "⚽", left: "5%", top: "5%" },
-  { title: "Futsal", emoji: "⚽", left: "40%", top: "5%" },
-  { title: "Badminton", emoji: "🏸", left: "10%", top: "35%" },
-  { title: "Volly", emoji: "🏐", left: "45%", top: "40%" },
-  { title: "Tennis", emoji: "🏓", left: "70%", top: "35%" },
-  { title: "Reading", emoji: "📖", left: "45%", top: "70%" },
-  { title: "Basketball", emoji: "🏀", left: "5%", top: "65%" },
-  { title: "Gaming", emoji: "🎮", left: "75%", top: "65%" },
-  { title: "Billiard", emoji: "🎱", left: "75%", top: "5%" },
-];
 
 export const AboutSection = () => {
   const constraintsRef = useRef(null);
