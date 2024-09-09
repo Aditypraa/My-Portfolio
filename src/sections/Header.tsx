54333480d36a0276bd6f2016275e8199a1fa6c00
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-09 09:25:48 +0700

    fix bug navbar and responsive and button project

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -9,9 +9,6 @@ export const Header = () => {
           Project
         </a>
         <a href="#" className="nav-item">
-          Experience
-        </a>
-        <a href="#" className="nav-item">
           About
         </a>
         <a
