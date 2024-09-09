bc352ea205224c0d417a37b044e86e5c4a23791d
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-09 08:24:12 +0700

    change testimonial to Work DONE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
-import { TestimonialsSection } from "@/sections/Testimonials";
+import { WorkSection } from "@/sections/Work";
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
       <HeroSection />
       <ProjectsSection />
       <TapeSection />
-      <TestimonialsSection />
+      <WorkSection />
       <AboutSection />
       <ContactSection />
       <Footer />
