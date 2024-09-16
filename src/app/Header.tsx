9859b63024c28863d5ace92ec0cecf925a52ef3c
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-17 00:26:25 +0700

    fixs : active navbar

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navbarItems = [
   { name: "Home", href: "/" },
@@ -8,11 +11,14 @@ const navbarItems = [
 ]
 
 export const Header = () => {
+
+  const pathname = usePathname();
+
   return (
     <div className="flex justify-center items-center sticky top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border-white/15 rounded-full bg-white/10 backdrop-blur">
         {navbarItems.map((item, index) => (
-          <Link key={index} href={item.href} className="nav-item">
+          <Link key={index} href={item.href} className={`${pathname === item.href ? 'nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900' : ''} nav-item`}>
             {item.name}
           </Link>
         ))}
