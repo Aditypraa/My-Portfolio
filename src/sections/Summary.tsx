e0f86ed38434a11343226d175614abcd371c2124
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-19 07:57:07 +0700

    add : animation transation in summary

--- src/sections/Summary.tsx
+++ src/sections/Summary.tsx
@@ -1,37 +1,46 @@
+'use client';
 import Image from 'next/image';
 import React from 'react';
 import sampulImage from '@/assets/images/Sampul.png';
 import fotoProfil from '@/assets/images/Ap.png';
+import { motion } from 'framer-motion';
 
 function SummarySection() {
   return (
-    <section className="">
-      <div className="px-5 mt-24 mx-auto flex flex-col">
+    <section className="py-16 lg:py-24">
+      <div className="px-5 mx-auto flex flex-col">
         <div className="lg:w-4/6 mx-auto">
-          <div className="rounded-lg h-64 overflow-hidden">
+          <motion.div
+            initial={{ opacity: 0, scale: 0, x: '100vw' }}
+            animate={{ opacity: 1, scale: 1, x: 0 }}
+            transition={{ type: 'spring', stiffness: 50, duration: 0.5 }}
+            className="rounded-lg h-64 overflow-hidden"
+          >
             <Image
               alt="content"
               className="object-cover object-center h-full w-full"
               src={sampulImage}
             />
-          </div>
-          <div className="flex flex-col sm:flex-row mt-10">
-            <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, scale: 0, x: '-100vw' }}
+            animate={{ opacity: 1, scale: 1, x: 0 }}
+            transition={{ type: 'spring', stiffness: 50, duration: 0.5 }}
+            className="flex flex-col justify-center items-center md:flex-row mt-10"
+          >
+            <div className="text-center sm:pr-8 sm:py-8">
               <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
                 <Image alt="profil" className="rounded-full" src={fotoProfil} />
               </div>
               <div className="flex flex-col items-center text-center justify-center">
-                <h2 className="font-medium title-font py-4 text-lg">
-                  Aditya Pratama
-                </h2>
+                <h2 className="font-serif text-base py-4">Aditya Pratama</h2>
                 <div className="w-32 h-1 bg-gradient-to-r from-emerald-300 to-sky-400 rounded mb-4"></div>
-                <p className="text-base">
-                  Web Developer | Informatic Engineering
-                </p>
+                <p className="font-serif text-base">Web Developer</p>
               </div>
             </div>
-            <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-              <p className="leading-relaxed mb-4 text-justify text-base lg:text-lg">
+            <div className="flex items-center justify-center  sm:mt-4 sm:pt-4 max-[380px]:mt-4 max-[380px]:pt-4">
+              <p className="text-justify text-base">
                 I am a Fresh Graduate with a Bachelor`s degree in Informatics
                 Engineering from, the Universitas 17 Agustus 1945 Surabaya, with
                 expertise in using JavaScript programming language frameworks. I
@@ -45,7 +54,7 @@ function SummarySection() {
                 contribution to every project I work on.
               </p>
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
